b7e83915b3c69013b423caacfc75c2e73e048b6a
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-07 21:34:15 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,4 @@
+let paragraph = "Cell"; 
+
+let message = ("Cell is the basic unit of life.It is a mass of cytoplam that is bound externally by a cell membrane.It's like the building block of life.They can be found in plants, animals and even microorganisms.It have a variety of organelles like nucleus,mitochondria,and ribosomes.The main function of cell is to carry out essential process.Each cell has a unique role.Cells work together to keep us alive and thriving.");
+console.log(paragraph + message);
